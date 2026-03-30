1e952b03c401afba35112d76c0ba67ebfbef54d3
arjawa123 <briexenv@gmail.com>
2026-03-30 22:10:47 +0800

    v1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.devicecontrol"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.1.3"
+        versionCode = 10
+        versionName = "1.1.4"
     }
 
     buildTypes {
